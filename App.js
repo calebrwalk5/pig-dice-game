7c94b47d839098f3b05d00d07bc2b488e70c9649
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2020-02-26 10:14:37 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+function rollDice() {
+	var d1 = Math.floor(Math.random() * 5);
+	var d2 = Math.floor(Math.random() * 5);
+	d1++;
+	d2++;
+	if (d2 == 1) {
+		document.getElementById("score").innerHTML = "You died";
+		document.getElementById("d1").innerHTML = "Die 1 is: " +d1;
+		document.getElementById("d2").innerHTML = "Die 2 is: " +d2;
+	} else if (d1 == 1) {
+		document.getElementById("score").innerHTML = "You died";
+		document.getElementById("d1").innerHTML = "Die 1 is: " +d1;
+		document.getElementById("d2").innerHTML = "Die 2 is: " +d2;
+	} else {
+		var score = d2 + d1;
+		document.getElementById("d1").innerHTML = "Die 1 is: " +d1;
+		document.getElementById("d2").innerHTML = "Die 2 is: " +d2;
+		document.getElementById("score").innerHTML = score;
+	}
+}
